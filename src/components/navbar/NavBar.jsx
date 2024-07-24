88d71f75826dddd788138b433334b90cc379b233
afeDayo <afedayo@gmail.com>
2024-07-24 09:53:08 +0100

    image testing

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./NavBar.css";
+import Navlogo from "../../../src/assets/EXTERNAL/TSA community 1.png";
 
 const NavBar = () => {
   return (
     <nav className="exnav">
       <div className="inner_nav">
-        <img src="../../../src/assets/EXTERNAL/TSA community 1.png" alt="TS_logo" />
+        <img src={Navlogo} alt="TS_logo" />
         <div className="first_a">
           <a href="#">Explore Community</a>
           <a href="#">Find Talent</a>
